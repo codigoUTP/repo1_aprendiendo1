142840cc5015188b72f3a57eb2c0f249b8102db5
codigoUTP <alfonsogallegost@gmail.com>
2021-08-10 09:52:42 -0500

    mensaje de otro commit para aprender

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo cruel y despiadado y descarado.");
+        System.out.println("Hoy me siento bien, por el día de hoy.");
     }
 }
